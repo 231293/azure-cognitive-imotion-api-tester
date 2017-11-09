448bd493cc9126cbc875cb14988e8083424d2010
Patil <Nilesh.Patil@emtecinc.com>
2017-11-09 16:46:53 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
 
+import { environment } from '../environments/environment'
+import { HttpHeaders } from '@angular/common/http';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'appsss';
+    @ViewChild('fileInput') fileInput;
+    selectedImage = "";
+    selectedImageName = "";
+    emotionGif = "";
+
+    constructor(private httpClient: HttpClient) {
+
+
+    }
+    upload(file) {
+
+        const options = {
+            headers: new HttpHeaders().append('Ocp-Apim-Subscription-Key', environment.Key1)
+                .append("Content-Type", "application/octet-stream")
+        };
+
+        this.httpClient.post(environment.emotionAPI, this.makeBlob(file), options)
+            .subscribe((res) => {
+                console.log(res);
+
+
+            }, (err) => {
+                console.log(err);
+            }, () => {
+                // complete
+            });
+    }
+
+    onFileChange(event) {
+        let reader = new FileReader();
+        if (event.target.files && event.target.files.length > 0) {
+            let file = event.target.files[0];
+
+            this.selectedImageName = file.name;
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                this.selectedImage = reader.result;
+                this.upload(this.selectedImage);
+            };
+        }
+    }
+
+    makeBlob(dataURL) {
+        var BASE64_MARKER = ';base64,';
+        if (dataURL.indexOf(BASE64_MARKER) == -1) {
+            var parts = dataURL.split(',');
+            var contentType = parts[0].split(':')[1];
+            var raw = decodeURIComponent(parts[1]);
+            return new Blob([raw], { type: contentType });
+        }
+        var parts = dataURL.split(BASE64_MARKER);
+        var contentType = parts[0].split(':')[1];
+        var raw = window.atob(parts[1]);
+        var rawLength = raw.length;
+
+        var uInt8Array = new Uint8Array(rawLength);
+
+        for (var i = 0; i < rawLength; ++i) {
+            uInt8Array[i] = raw.charCodeAt(i);
+        }
+
+        return new Blob([uInt8Array], { type: contentType });
+    }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    emotionAPI: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?',
+    Key1: 'fb6e320d38d24f3db09440c413ff79c8',
+    Key2: '84b8cd362bf44e92a352cc44b2061293'
 };
